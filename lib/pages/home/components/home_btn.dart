b4b540d8e4ac34ab884dd53d7bffb574e174da49
unknown <andrecruz2007@hotmail.com>
2024-01-05 15:55:53 -0300

    create page home

---
+++ lib/pages/home/components/home_btn.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:plannerfy_desktop/utility/app_config.dart';
+
+class HomeButton extends StatelessWidget {
+  final String texto;
+  final Function login;
+
+  const HomeButton({Key? key, required this.texto, required this.login})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        login();
+      },
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        minimumSize: Size(
+          MediaQuery.of(context).size.width * 0.9,
+          60,
+        ),
+        primary: markPrimaryColor,
+      ),
+      child: Text(
+        texto,
+        style: const TextStyle(
+            fontSize: 25, color: Colors.white, fontFamily: primaryFont),
+      ),
+    );
+  }
+}
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,15 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:plannerfy_desktop/pages/home/components/home_btn.dart';
+import 'package:plannerfy_desktop/utility/app_config.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? selectedValue;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          const Text('oi'),
-        ],
+    List<DropdownMenuItem<String>> list = [
+      DropdownMenuItem<String>(
+        value: 'Documentos',
+        child: Text('Documentos'),
+      ),
+      DropdownMenuItem<String>(
+        value: 'Contrato social',
+        child: Text('Contrato social'),
+      ),
+      DropdownMenuItem<String>(
+        value: 'DRE Contabil',
+        child: Text('DRE Contabil'),
+      ),
+      DropdownMenuItem<String>(
+        value: 'Balanço',
+        child: Text('Balanço'),
+      ),
+      DropdownMenuItem<String>(
+        value: 'Balancete',
+        child: Text('Balancete'),
+      ),
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                color: markPrimaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'lib/assets/images/Logo2.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+              width: 300.0,
+              padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: DropdownButton<String>(
+                underline: Container(),
+                items: list,
+                hint: Text(
+                  'Tipo de Arquivo',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+                value: selectedValue,
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value;
+                  });
+                },
+                isExpanded: true,
+              ),
+            ),
+                        // const SizedBox(height: 20),
+                        // const SizedBox(height: 20),
+                        // const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 70),
+                          child: HomeButton(
+                            texto: "Enviar",
+                            login: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
