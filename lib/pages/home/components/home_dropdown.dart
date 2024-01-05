01b13d9e8fd934b7744cc1354060c3a9dc00af7b
unknown <andrecruz2007@hotmail.com>
2024-01-05 17:10:18 -0300

    ajusts home

---
+++ lib/pages/home/components/home_dropdown.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomDropdown extends StatelessWidget {
+  final List<String> items;
+  final String? hintText;
+  final Function(String?) onChanged;
+
+  const CustomDropdown({
+    Key? key,
+    required this.items,
+    required this.onChanged,
+    this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.white,
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          isExpanded: true,
+          items: items.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  title: Center(child: Text(value)),
+                ),
+              ),
+            );
+          }).toList(),
+          onChanged: onChanged,
+          hint: hintText != null
+              ? Center(
+                  child: Text(
+                    hintText!,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,148 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:plannerfy_desktop/pages/home/components/home_btn.dart';
+import 'package:plannerfy_desktop/pages/home/components/home_dropdown.dart';
+import 'package:plannerfy_desktop/pages/home/components/home_dropdown2.dart';
+import 'package:plannerfy_desktop/pages/login/login_page.dart';
 import 'package:plannerfy_desktop/utility/app_config.dart';
 
-import '../login/login_page.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String? selectedValue;
-  List<DropdownMenuItem<String>> list = [
-    DropdownMenuItem<String>(
-      value: 'Documentos',
-      child: Text('Documentos'),
-    ),
-    DropdownMenuItem<String>(
-      value: 'Contrato social',
-      child: Text('Contrato social'),
-    ),
-    DropdownMenuItem<String>(
-      value: 'DRE Contabil',
-      child: Text('DRE Contabil'),
-    ),
-    DropdownMenuItem<String>(
-      value: 'Balanço',
-      child: Text('Balanço'),
-    ),
-    DropdownMenuItem<String>(
-      value: 'Balancete',
-      child: Text('Balancete'),
-    ),
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: markPrimaryColor,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'lib/assets/images/Logo2.png',
-                          width: 150,
-                          height: 150,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(
+                  color: markPrimaryColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'lib/assets/images/Logo2.png',
+                        width: 150,
+                        height: 150,
+                      ),
+                      const SizedBox(height: 20),
+                      const SizedBox(height: 20),
+                      const SizedBox(height: 20),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 150),
+                        child: CustomDropdown(
+                          items: const [
+                            'Estrelar Ltda',
+                            'MegaTech Solutions Inc',
+                            'Global Foods Group',
+                            'InnovateWare Co',
+                            'Visionary Motors Corporation',
+                            'Evergreen Investments LLC',
+                            'Quantum Innovations Ltd',
+                            'Pacific Crest Enterprises',
+                          ],
+                          onChanged: (value) {
+                            // Implemente a lógica para quando um valor for selecionado
+                          },
+                          hintText: 'Selecione Empresa',
                         ),
-                        const SizedBox(height: 20),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            // const SizedBox(height: 20),
-                            // const SizedBox(height: 20),
-                            // const SizedBox(height: 20),
-                            Padding(
-                              padding: const EdgeInsets.all(70.0),
-                              child: Container(
-                                padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.grey,
-                                    width: 1.0,
-                                  ),
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                                child: DropdownButton<String>(
-                                  underline: Container(),
-                                  items: list,
-                                  hint: Text(
-                                    'Tipo de Arquivo',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      decoration: TextDecoration.none,
-                                      fontSize: 16.0,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  value: selectedValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      selectedValue = value;
-                                    });
-                                  },
-                                  isExpanded: true,
-                                ),
-                              ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Center(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 150),
+                            child: CustomDropdown2(
+                              items: const [
+                                'Documentos',
+                                'Contrato social',
+                                'DRE Contabil',
+                                'Balanço',
+                                'Balancete',
+                              ],
+                              onChanged: (value) {
+                                // Implemente a lógica para quando um valor for selecionado
+                              },
+                              hintText: 'Tipo de Arquivo',
                             ),
-
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 70),
-                              child: HomeButton(
-                                texto: "Enviar",
-                                login: () {},
-                              ),
+                          ),
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 130),
+                            child: HomeButton(
+                              texto: "Enviar",
+                              login: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const HomePage(),
+                                  ),
+                                );
+                              },
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            Positioned(
-              top: 20,
-              right: 20,
-              child: IconButton(
-                onPressed: () {
-                  // Lógica para o logout
-                  Navigator.pop(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginPage(),
-                    ),
-                  ); // Volta para a tela anterior (ou tela de login)
-                },
-                icon: Icon(Icons.logout),
-                color: Colors.black, // Cor do ícone
               ),
+            ],
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: IconButton(
+              onPressed: () {
+                // Lógica para o logout
+                Navigator.pop(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                ); // Volta para a tela anterior (ou tela de login)
+              },
+              icon: const Icon(Icons.logout),
+              color: Colors.black, // Cor do ícone
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
